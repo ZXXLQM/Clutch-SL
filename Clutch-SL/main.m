e35811b30b60ffbdf0a8ade602dbada3cf698949
曾祥翔 <zengxiangxiang@cengxiangxiangdeMacBook-Pro.local>
2017-11-14 17:03:57 +0800

    Initialization Project

---
+++ Clutch-SL/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Clutch-SL
+//
+//  Created by 曾祥翔 on 2017/11/14.
+//  Copyright © 2017年 曾祥翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        
+        if (getuid() != 0) {
+            
+        }
+        
+        return 0;
+    }
+}
